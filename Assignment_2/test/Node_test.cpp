0979208f6346835dbc99b0c85a234341e18eb351
Hannes Kuchelmeister <hannes@kuchelmeister.org>
2021-01-02 20:44:24 +0100

    add node tests

--- Assignment_2/test/Node_test.cpp
+++ Assignment_2/test/Node_test.cpp
@@ -2,10 +2,18 @@
 
 #include <lib/Node.h>
 
-TEST(NodeTest, DummyNode) {
+TEST(NodeTest, DummyNode_smaller_int) {
     Node * n = Node::Dummy();
     ASSERT_FALSE(n->is_smaller_than(std::numeric_limits<int>::max()));
     ASSERT_FALSE(n->is_smaller_than(std::numeric_limits<int>::min()));
+}
+
+TEST(NodeTest, DummyNode_smaller_node) {
+    Node * n = Node::Dummy();
+    Node * max = new Node(std::numeric_limits<int>::max());
+    Node * min = new Node(std::numeric_limits<int>::min());
+    ASSERT_FALSE(n->is_smaller_than(max));
+    ASSERT_FALSE(n->is_smaller_than(min));
 }
 
 
