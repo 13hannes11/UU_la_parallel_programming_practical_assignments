3ad63eb3a8dcaf305d605a64328627c04380b8c3
Hannes Kuchelmeister <hannes@kuchelmeister.org>
2021-01-02 16:36:11 +0100

    add add test to fine_list

--- Assignment_2/test/FineSet_test.cpp
+++ Assignment_2/test/FineSet_test.cpp
@@ -3,7 +3,13 @@
 #include <lib/FineList.cpp>
 
 
-TEST(FineSetTest, Whatever2) {
+TEST(FineSetTest, add1_add1) {
+    FineSet * fl = new FineSet();
+
+    ASSERT_TRUE(fl->add(1));
+    ASSERT_FALSE(fl->add(1));
+}
+
     FineSet * fl = new FineSet();
     //ASSERT_TRUE(fl->add(1));
 }
