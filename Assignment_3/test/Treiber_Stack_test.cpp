f7339cbc55a749ab2cfbd6e8f4aee7846622f39a
Hannes Kuchelmeister <hannes@kuchelmeister.org>
2021-01-10 22:04:02 +0100

    add basic tests for treiber stack

--- Assignment_3/test/Treiber_Stack_test.cpp
+++ Assignment_3/test/Treiber_Stack_test.cpp
@@ -1,9 +1,41 @@
 #include <gtest/gtest.h>
 
 #include <lib/TreiberStack.h>
+#include <iostream>
 
 
-TEST(TreiberStackTest, SimpleEmptyPrint) {
+TEST(TreiberStackTest, EmptyPrint) {
     TreiberStack * stack = new TreiberStack(new ADTOperationQueue());
     EXPECT_EQ("", stack->print_stack());
+}
+
+TEST(TreiberStackTest, Push) {
+    TreiberStack * stack = new TreiberStack(new ADTOperationQueue());
+    stack->push(1);
+    EXPECT_EQ("1", stack->print_stack());
+}
+
+TEST(TreiberStackTest, PushPop) {
+    TreiberStack * stack = new TreiberStack(new ADTOperationQueue());
+    stack->push(1);
+    EXPECT_EQ(1, stack->pop());
+    EXPECT_EQ("", stack->print_stack());
+}
+
+TEST(TreiberStackTest, SizeZero) {
+    TreiberStack * stack = new TreiberStack(new ADTOperationQueue());
+    EXPECT_EQ(0, stack->size());
+}
+
+TEST(TreiberStackTest, SizeAfterPush) {
+    TreiberStack * stack = new TreiberStack(new ADTOperationQueue());
+    stack->push(1);
+    EXPECT_EQ(1, stack->size());
+}
+
+TEST(TreiberStackTest, SizeAfterPushPop) {
+    TreiberStack * stack = new TreiberStack(new ADTOperationQueue());
+    stack->push(1);
+    stack->pop();
+    EXPECT_EQ(0, stack->size());
 }
